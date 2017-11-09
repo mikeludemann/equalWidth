b916dfa9b23650a4978cbbb6a5e12195356c33b9
Mike Ludemann <mike-ludemann@hotmail.de>
2017-11-09 17:23:10 +0100

    First Commit with all data informations

---
+++ README.md
@@ -0,0 +1,9 @@
+# equalWidth
+
+Set the equal width in all elements in a main / parent element
+
+## Example
+
+```code
+equalWidth(".main",".sub");
+```
---
+++ equalWidth.js
@@ -0,0 +1,14 @@
+function equalWidth(main, submain) {
+    'use strict';
+
+    var elements  = main.find(submain),
+        widths = [],
+        maximumWidth = null;
+
+    elements.each(function () {
+        $(this).removeAttr('style');
+        widths.push($(this).width());
+    });
+    maximumWidth = Math.max.apply(null, widths);
+    elements.width(maximumHeight);
+}
